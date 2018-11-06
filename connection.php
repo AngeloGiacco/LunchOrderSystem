77eb6391df1919e5f2b3148dca41228cb843430f
Angelo Giacco <giacco.angelo.s@gmail.com>
2018-11-06 19:36:37 +0000

    Add files via upload

---
+++ connection.php
@@ -0,0 +1,16 @@
+<?php
+$servername = "http://fdb12.awardspace.net/";
+$username = "giacco.a@oundleschool.org";
+$password = "7eDrHxZsZVSExFn";
+$dbname = "2829205_pupils";
+
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected successfully <br>";
+    }
+catch(PDOException $e)
+    {
+    echo "Connection failed: ".$e->getMessage();
+    }
+?>
