dfd331dfea8b774a130e9146dbda129b61b3ff29
Angelo Giacco <giacco.angelo.s@gmail.com>
2018-11-12 22:03:24 +0000

    Update add_user.php

--- add_user.php
+++ add_user.php
@@ -1,19 +1,21 @@
 <?php
-header('Location: signUp.html');
+header('location: index.html');
 array_map("htmlspecialchars", $_POST);
 include_once("connection.php");
-
-$stmt = $conn->prepare("INSERT INTO pupils (StudentID,Surname,Forename,House,email,password)VALUES (null,:surname,:forename,:house,:email,:password)");
-
-$stmt->bindParam(':forename', $_POST["forename"]);
-$stmt->bindParam(':surname', $_POST["surname"]);
-$stmt->bindParam(':house', $_POST["house"]);
-$stmt->bindParam(':email', $_POST["email"]);
-$stmt->bindParam(':password', $_POST["psw"]);
-$stmt->execute();
+try{
+	$stmt = $conn->prepare("INSERT INTO 'pupils' (StudentID,Surname,Forename,House,email,password)VALUES (null,:surname,:forename,:house,:email,:password)");
+	echo "<script>alert.(".$POST["forename"].")</script>";
+	$stmt->bindParam(':forename', $_POST["forename"]);
+	$stmt->bindParam(':surname', $_POST["surname"]);
+	$stmt->bindParam(':house', $_POST["house"]);
+	$stmt->bindParam(':email', $_POST["email"]);
+	$stmt->bindParam(':password', $_POST["psw"]);
+	$stmt->execute();
+	$conn=null;
+}
 catch(PDOException $e)
 	{
 		echo "error".$e->getMessage();
 	}
-$conn=null;
 ?>
+
