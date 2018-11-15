bcf72e93de22076170de226f5ff65fa73be9f74d
Angelo Giacco <giacco.angelo.s@gmail.com>
2018-11-15 08:44:51 +0000

    Update authenticate.php

--- authenticate.php
+++ authenticate.php
@@ -5,7 +5,7 @@ array_map("htmlspecialchars", $_POST);
 include_once("connection.php");
 try {
   $stmt = $conn->prepare("SELECT password FROM pupils WHERE email = :email");
-  $stmt.bindParam(':email',$_POST["email"]);
+  $stmt->bindParam(':email',$_POST["email"]);
   $stmt.execute();
   if ($_POST["email"] = "admin " and $_POST["psw"] = "nimda") {// if user login information correct
     echo "successful admin login";
