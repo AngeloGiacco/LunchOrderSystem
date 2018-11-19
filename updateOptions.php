bd2b85ec1a8ccde83fbdc4d320eb077edf56f802
Angelo Giacco <giacco.angelo.s@gmail.com>
2018-11-19 12:39:17 +0000

    Update updateOptions.php

--- updateOptions.php
+++ updateOptions.php
@@ -8,7 +8,7 @@
   <h1>Current options</h1>
   <?php
   	include_once('connection.php');
-  	$stmt = $conn->prepare("SELECT * FROM Food"); //select all the food options from the food table
+  	$stmt = $conn->prepare("SELECT * FROM food"); //select all the food options from the food table
   	$stmt->execute();
   	while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) //print all the options
   		{
