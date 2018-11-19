96a0cb9306d376e901759bbde72d11ccfbd6bd69
Angelo Giacco <giacco.angelo.s@gmail.com>
2018-11-19 11:24:32 +0000

    Update connection.php

--- connection.php
+++ connection.php
@@ -7,7 +7,6 @@ $dbname = "2829205_pupils";
 try {
     $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "Connected successfully <br>";
     }
 catch(PDOException $e)
     {
