bd006586ac4d4088b2218badbc3a3b248641e617
Angelo Giacco <giacco.angelo.s@gmail.com>
2019-03-19 12:38:51 +0000

    Update authenticate.php

--- authenticate.php
+++ authenticate.php
@@ -30,24 +30,6 @@
           </script><?php
           exit();
       }
-array_map("htmlspecialchars", $_POST);
-include_once("connection.php");
-try {
-  if ($_POST["uname"] == "admin" and $_POST["psw"] == "nimda") {// if user login information correct
-    header("Location: updateOptions.php");
-    exit();
-  } else {
-    $stmt = $conn->prepare("SELECT password FROM pupils WHERE email = :email");
-    $stmt->bindParam(':email',$_POST["uname"]);
-    $stmt->execute();
-    $row = $stmt->fetch(PDO::FETCH_ASSOC);
-    if ($row["password"] = $_POST["psw"]) {
-      $_SESSION["email"] = $_POST["uname"];
-      header("Location: order.php");
-      exit();
-    } else {
-      header("Location:index.php");
-      exit();
     }
     $conn=null;
   }
